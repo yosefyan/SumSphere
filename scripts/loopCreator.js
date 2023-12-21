349bfb70146a6da06c3878eb7e4bfc2948ea5704
Yosef Yan <145193433+yosefyan@users.noreply.github.com>
2023-12-21 14:36:13 +0200

    Add files via upload

--- scripts/loopCreator.js
+++ scripts/loopCreator.js
@@ -3,7 +3,7 @@ const loopCreator = (container, ele, arr, classes) => {
     const isLastColumn = (i + 1) % 4 === 0;
     container.innerHTML += `<${ele} class="${
       a === "="
-        ? "bg-green-500/25 col-span-2"
+        ? "bg-green-500/25 col-span-4"
         : isLastColumn || Math.floor(i / 4) == 0
         ? "bg-blue-400/25"
         : ""
